ccea307b99a6429887afd2d261d1799901ca23ac
Charles2205 <cewudzie0@gmail.com>
2023-10-18 16:33:11 +0000

    Update : done with console

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,51 +1,64 @@
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import axios from "axios";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import "./content.css";
+import { useEffect, useState } from "react";
 
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
+const Search = () => {
+  const [input, setInput] = useState("");
 
-import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
-import './content.css'
-import { useEffect } from 'react';
+  useEffect(() => {
+    async function requestData() {
+      try {
+        if (input.trim().length > 0) {
+          const res = await axios.get(`https://bible-api.com/${input}`);
+          console.log(res.data);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    requestData();
+  }, [input]);
 
-
-useEffect(()=>{
-    async function ApiRequest(){}
-})
-
-const Search =()=>{
-
-    return(
+  // setting up input state
+  const onChangeInput = (e) => {
+    setInput(e.target.value);
+  };
+  // click event
+  const btnSearch = () => {
+    console.log(input);
+  };
+  return (
     <>
-    
-    
-    <center>
+      <center>
+        <Container className="my-5">
+          <InputGroup className="mb-3">
+            <Button
+              onClick={btnSearch}
+              variant="outline-primary"
+              id="button-addon1"
+            >
+              Search
+            </Button>
+            <Form.Control
+              aria-label="Example text with button addon"
+              aria-describedby="basic-addon1"
+              onChange={onChangeInput}
+              value={input}
+            />
+          </InputGroup>
 
-    <Container className='my-5'>
+          <div className="content">
+            <span className="Ref">Ref</span> <br />
+            <span className="Verse">Verse</span>
+          </div>
+        </Container>
+      </center>
+    </>
+  );
+};
 
-    <InputGroup className="mb-3">
-        <Button variant="outline-primary" id="button-addon1">
-            Search
-        </Button>
-        <Form.Control
-          aria-label="Example text with button addon"
-          aria-describedby="basic-addon1"
-        />
-      </InputGroup>
-
-      <div className='content'>
-            <span>HEllo</span>
-      </div>
-    </Container>
-
-    </center>
-    
-    
-        </>
-    )
-
-
-
-}
-
-
-export default Search
+export default Search;
